1d1c6b518077f5ac9f4f1827b521305600ef4127
Brian Foody <brian.foody@tribalddb.co.uk>
2014-09-13 11:18:33 +0800

    updated heroku config

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var serveStatic = require('serve-static')
 
 var app = connect()
 
-app.use(serveStatic('dist/', {'index': ['index.html', 'default.htm']}))
-app.listen(8080)
+var port = process.env.PORT || 8080;
 
-console.log('Listening on port 8080.');
+app.use(serveStatic('dist/', {'index': ['index.html', 'default.htm']}))
+app.listen(port)
